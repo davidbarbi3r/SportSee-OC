b6dd6b288000f015a93a0ee1107e80636958b951
davidbarbi3r <davidbarbi3r@gmail.com>
2023-06-28 14:26:26 +0200

    :lipstick: correct layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,3 +1,4 @@
+import './Layout.css'
 import { Outlet } from "react-router-dom"
 import Header from "./components/Header.jsx"
 import Drawer from "./components/Drawer"
@@ -6,8 +7,10 @@ function Layout() {
   return (
     <div>
         <Header />
-        <Drawer />
-        <Outlet />
+        <div className='layout-main'>
+            <Drawer />
+            <Outlet />
+        </div>
     </div>
   )
 }
