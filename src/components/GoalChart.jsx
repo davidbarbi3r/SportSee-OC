d678cf13d24bf07e9ea2b6e812cd01f269459ae8
davidbarbi3r <davidbarbi3r@gmail.com>
2023-07-07 11:28:09 +0200

    :lipstick: Add title and innercircle to GoalChart

--- src/components/GoalChart.jsx
+++ src/components/GoalChart.jsx
@@ -1,3 +1,4 @@
+import './GoalChart.css';
 import { PieChart, Pie, ResponsiveContainer, Label, Cell } from "recharts";
 import { formatGoal } from "../service/formatData";
 
@@ -35,29 +36,39 @@ const CustomLabel = ({ viewBox, value }) => {
 export default function GoalChart(props) {
   const data = formatGoal(props.score);
 
+  if (!data) {
+    return <div>ERROR</div>;
+  }
+
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <PieChart width={400} height={400}>
-        <Pie
-          data={data}
-          dataKey="value"
-          cx="50%"
-          cy="50%"
-          innerRadius={80}
-          outerRadius={100}
-          fill="red"
-        >
-            <Cell
-                key={`cell-0`}
-                fill='#FF0000'
+    <div className="goal-chart-container">
+      <h3>Score</h3>
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={data}
+            dataKey="value"
+            cx="50%"
+            cy="50%"
+            innerRadius={80}
+            outerRadius={100}
+            fill="red"
+          >
+            <Cell key={`cell-0`} fill="#FF0000" />
+            <Cell key={`cell-1`} fill="#FFF" />
+            <Label
+              content={<CustomLabel value={data[0].value * 100} />}
+              position="center"
             />
-            <Cell
-                key={`cell-1`}
-                fill='#FFF'
+          </Pie>
+          <Pie data={[{name: '', value:1}, {name: '', value:1}]} cx="50%" cy="50%" innerRadius={0} outerRadius={80} fill="#fff">
+          <Label
+              content={<CustomLabel value={data[0].value * 100} />}
+              position="center"
             />
-            <Label content={<CustomLabel value={data[0].value * 100} />} position="center" />
-        </Pie>
-      </PieChart>
-    </ResponsiveContainer>
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
